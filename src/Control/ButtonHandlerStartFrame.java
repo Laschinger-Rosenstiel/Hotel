71e24d7bd65bcd1ff4ea89db49b24061059b8468
Stepuna <zerwas2000@gmx.de>
2014-01-08 11:07:09 +0100

    StartFrame

---
+++ src/Control/ButtonHandlerStartFrame.java
@@ -0,0 +1,41 @@
+package Control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import GUI.StartFrame;
+
+
+
+	public class ButtonHandlerStartFrame implements ActionListener 
+	{
+		StartFrame s;
+		JPanel panel;
+
+		public ButtonHandlerStartFrame(StartFrame s)
+		{
+			this.s = s;
+
+		}
+
+		public ButtonHandlerStartFrame(JPanel panel)
+		{
+			this.panel = panel;
+		}
+
+		public void actionPerformed(ActionEvent e) 
+		{
+			System.out.println("Das Ereignis hat den Wert: " +e.getActionCommand());
+			if (e.getActionCommand().equals("Zimmer buchen"))
+			{
+				BookZimmer bz = new BookZimmer();
+				System.out.println("Button2");
+				s.launchStartFrame(BookZimmer.getP1());
+				System.out.println("Button gedrückt");
+			}
+
+
+
+		}
+	}
+

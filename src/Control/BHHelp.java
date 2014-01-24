3d27902646c24cdf2f13c6f44d670834fc0d5565
WLash <wlaschinger@live.de>
2014-01-24 18:43:14 +0100

    asdf

--- src/Control/BHHelp.java
+++ src/Control/BHHelp.java
@@ -1,8 +1,16 @@
 package Control;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class BHHelp {
 
@@ -33,6 +41,11 @@ public class BHHelp {
 			if (cGeb.after(cNow)) {
 				throw new GUIException("Geburtsdatum überprüfen!");
 			}
+	}
+	
+	public String setSQLDate(Date date) {
+		SimpleDateFormat Sql =new SimpleDateFormat("yyyy-MM-dd");
+		return Sql.format(date);
 	}
 	
 	public void checkStringEmpty(String check) throws GUIException {
@@ -97,5 +110,39 @@ public class BHHelp {
 			{
 				throw new GUIException("Falsches Passwort");
 			}	
+	}
+	
+	public void writeDB(String SQLquery) 
+	{ 
+		try 
+		{ 
+			String sDbDriver=null, sDbUrl=null, sUsr="", sPwd=""; 
+			// set access data for database connection 
+			sDbDriver = "com.mysql.jdbc.Driver"; 
+			// url of data base scheme (f.e. jdbc:mysql://localhost:3306/test) 
+			sDbUrl = "jdbc:mysql://localhost:3306/Hotel"; 
+			// user name (f.e. root) 
+			sUsr = "root"; 
+			// password (f.e. init) 
+			sPwd = "init"; 
+			// select fitting database driver and connect 
+			Class.forName( sDbDriver ); 
+			Connection cn = DriverManager.getConnection( sDbUrl, sUsr, sPwd ); 
+			Statement st = cn.createStatement(); 
+			// insert resp. delete entry 
+			st.execute(SQLquery); 
+			// to avoid side effects close connection 
+			st.close(); 
+			cn.close(); 
+		} 
+		catch (SQLException ex) 
+		{ 
+			JOptionPane.showMessageDialog(new JFrame(),ex.getMessage()); 
+		} 
+		catch( ClassNotFoundException ex ) 
+		{ 
+			JOptionPane.showMessageDialog(new JFrame(),ex.getMessage()); 
+		} 
+		
 	}
 }
